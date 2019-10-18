49b55f828b7e4adc10778c08e03b41cd76452079
JulesDisch <53347157+JulesDisch@users.noreply.github.com>
2019-10-18 02:57:47 -0400

    made extensive CSS edits:     added a background image     added individual crystal images     added style settings for hover

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Unit-4-Game
+The crystal game uses JavaScript for the logic and jQuery to manipulate the HTML, as well as a custom CSS.
+The most time-consuming part was editing the images to appear 3-D to pop off the page.
+Directions for game play can be found in the index file.
+The deployed version can be found at hhttps://julesdisch.github.io/Unit-4-Game/index.html.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
-// global variables
+    // global variables
+
+    var crystalPics = [
+        "octogon.png", "square.png", "quartz.png", "teardropped.png"];
     var counter = 0;
     var wins = 0;
     var losses = 0;
     var targetNumber;
-    
-// create target number
+
+    // create target number
     function newTarget() {
         targetNumber = Math.floor(Math.random() * 100) + 19;
         $("#number-to-guess").text(targetNumber);
@@ -14,8 +17,9 @@ $(document).ready(function () {
     // clear the divs
     function clear() {
         $("#number-to-guess").empty();
-        $("#crystals").empty();   
-      }
+        $("#crystals").empty();
+        $("#crystal-score").empty();
+    }
     // restart the game
     function reset() {
         clear();
@@ -24,98 +28,79 @@ $(document).ready(function () {
         counter = 0;
     }
 
-
+    // create the number options for the crystals that don't repeat
     function crystalNumbers() {
         var numberOptions = new Array(4);
         var j = 0;
         var min = 1;
         var max = 12;
-
         while (j < numberOptions.length) {
             var randnum = Math.floor(Math.random() * (max - min + 1) + min);
-
             var found = false;
-
             for (var i = 0; i < numberOptions.length; i++) {
                 if (numberOptions[i] === randnum) {
                     found = true;
                     break;
                 }
             }
-
             if (!found) {
                 numberOptions[j] = randnum;
                 j++;
             }
         }
-
-
-
         console.log(numberOptions);
-
 
         // Next we create a for loop to create crystals for every numberOption.
         for (var i = 0; i < numberOptions.length; i++) {
 
             // For each iteration, we will create an imageCrystal
             var imageCrystal = $("<img>");
-
-            // First each crystal will be given the class ".crystal-image".
-            // This will allow the CSS to take effect.
             imageCrystal.addClass("crystal-image");
 
             // Each imageCrystal will be given a src link to the crystal image
-            imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
+            imageCrystal.attr("src", "assets/images/" + crystalPics[i]);
             // Each imageCrystal will be given a data attribute called data-crystalValue.
             // This data attribute will be set equal to the array value.
             imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
             // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
             $("#crystals").append(imageCrystal);
-
         }
 
 
+        // function for modal to work
+        function countModal() {
+            var countModal = document.getElementById("counter");
+            var contBtn = document.getElementById("continue");
+            var quitBtn = document.getElementById("quit");
+            countModal.style.display = "block";
+            contBtn.onclick = function () {
+                countModal.style.display = "none";
+                reset();
+            }
+            quitBtn.onclick = function () {
+                countModal.style.display = "none";
+                $(".crystal-image").off("click");
+                $("#number-to-guess").empty();
+                $("#crystal-score").empty();
+            }
+        }
 
-        // This time, our click event applies to every single crystal on the page. Not just one.
         $(".crystal-image").on("click", function () {
-
-            // Determining the crystal's value requires us to extract the value from the data attribute.
-            // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-            // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-            // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
             var crystalValue = ($(this).attr("data-crystalvalue"));
             crystalValue = parseInt(crystalValue);
-            // We then add the crystalValue to the user's "counter" which is a global variable.
-            // Every click, from every crystal adds to the global counter.
             counter += crystalValue;
-            alert("New score: " + counter);
+            $("#crystal-score").text(counter);
             if (counter === targetNumber) {
-                alert("You win!");
+                $("#winOrLose").text("You win!");
+                countModal();
                 wins++;
                 $("#winsText").text(wins);
-                playAgain = confirm("Would you like to play again?");
-                if (playAgain === true) {
-                    reset();
-                }
-                else {
-                    return;
-                }
             }
-
             else if (counter >= targetNumber) {
-                alert("You lose!!");
+                $("#winOrLose").text("You lose!");
+                countModal();
                 losses++;
                 $("#lossesText").text(losses);
-                playAgain = confirm("Would you like to play again?");
-                if (playAgain === true) {
-                    reset();
-                }
-                else {
-                    return;
-                }
             }
         })
     };
